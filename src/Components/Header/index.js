8d5fe281b0d4d23740212afd7adf042eb9453b9b
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-02-04 13:06:34 -0300

    feat: Header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,3 +1,32 @@
+import './style.css'
+import { IoIosMenu } from 'react-icons/io'
+import { CiSettings } from 'react-icons/ci'
+import { BsTicketPerforated } from 'react-icons/bs'
+import NameUser from '../NameUser'
+
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <div>
+      <div className="card-profile">
+        <div className="card-dados">
+          <div className="card-img-profile">
+            <img src="i" />
+          </div>
+          <button className="btn-card">Minhas Atividades</button>
+        </div>
+        <div className="card-menu">
+          <div>
+            <BsTicketPerforated />
+          </div>
+          <div>
+            <IoIosMenu />
+          </div>
+          <div>
+            <CiSettings />
+          </div>
+        </div>
+      </div>
+      <NameUser />
+    </div>
+  )
 }
