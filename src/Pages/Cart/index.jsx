64f2e7ac70bd243c28aafd1a0e372b6ac7467cc2
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-02-13 15:34:41 -0300

    Feat: Cart

---
+++ src/Pages/Cart/index.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function Cart() {
+
+  return (
+    <div className="App">
+      <h1>Lista de Produtos</h1>
+    </div>
+  );
+}
+
+export default Cart
