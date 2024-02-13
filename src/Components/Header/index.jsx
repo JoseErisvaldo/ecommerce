5b7055bad9fbd93752f9ba531508480547965aa2
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-02-13 15:32:34 -0300

    feat: name user Header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -11,7 +11,6 @@ export default function Header() {
   const handelLogout = () => {
     logout()
   }
-
   return (
     <div>
       <div className="card-profile">
--- src/Components/NameUser/index.jsx
+++ src/Components/NameUser/index.jsx
@@ -1,9 +1,25 @@
 import './style.css'
-
+import React, {useContext, useEffect, useState} from 'react'
+import { AuthContext } from '../../Contexts/Login'
+import supabase from '../../supabaseClient'
 export default function NameUser() {
+  const {user} = useContext(AuthContext)
+  const [userBd, setUserBd] =useState([])
+  async function loadingUser () {
+    const {data, error} = await supabase
+    .from('users')
+    .select('*')
+    setUserBd(data)
+  } 
+  useEffect(() => {
+    loadingUser()
+  },[])
+
+  const resFilter = userBd.find(item => item.email === user.email)
+
   return (
     <div className="card-name-user">
-      <h1>Olá, José !</h1>
+      <h1>Olá,  {resFilter ? resFilter.nameUser : 'User Desconhecido !!' }</h1>
     </div>
   )
 }
