0bf6f152bb58cc5e3a6cbadb69f4e73b0e4faa7a
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-02-13 15:38:19 -0300

    fix: context

--- src/Contexts/Login/index.jsx
+++ src/Contexts/Login/index.jsx
@@ -1,44 +1,70 @@
-import React, {createContext, useEffect, useState} from "react";
+// AuthProvider.js
+
+import React, { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import supabase from "../../supabaseClient";
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
-  const navigate = useNavigate()
-  const [user, setUsers] = useState(null)
-  const [loading, setLoading] = useState(true)
+export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    const recoveredUser = localStorage.getItem('user')
+    const recoveredUser = localStorage.getItem("user");
 
-    if(recoveredUser) {
-      setUsers(JSON.parse(recoveredUser))
+    if (recoveredUser) {
+      setUser(JSON.parse(recoveredUser));
     }
 
-    setLoading(false)
-  },[])
+    setLoading(false);
+  }, []);
 
-
-  const login  = (email, password) => {
-    const resUser = {
-      id: 1,
-      email: email
+  const login = async (email) => {
+    try {
+      const resUser = {
+        email: email,
+      };
+  
+      setUser(resUser);
+  
+      // Carrega os dados do usuário do banco de dados após o login
+      const { data, error } = await supabase
+        .from("users")
+        .select("*")
+        .eq("email", resUser.email);
+  
+      if (error) {
+        console.error("Error loading user data:", error);
+      } else if (data && data.length > 0) {
+        setUserData(data[0]?.admin);
+        console.log(data[0]?.admin);
+      } else {
+        console.warn("User not found in the database.");
+      }
+  
+      localStorage.setItem("user", JSON.stringify(resUser));
+      localStorage.setItem('admin', JSON.stringify(data[0]?.admin))
+      navigate("/");
+    } catch (error) {
+      console.error("Error during login:", error);
     }
-
-    localStorage.setItem('user',JSON.stringify(resUser))
-
-    if(password === '123')
-    setUsers(resUser)
-    navigate('/')
-  }
+  };
 
   const logout = () => {
-    setUsers(null)
-    localStorage.removeItem('user')
-    navigate('/login')
-  }
-  return(
-    <AuthContext.Provider value={{ autenticado: !!user, login, loading, logout}}>
+    setUser(null);
+    setUserData(null); 
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ autenticado: !!user, user, userData, login, loading, logout }}
+    >
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,11 @@
-import { Link } from 'react-router-dom'
 import Announcement from '../../Components/Announcement'
 import Header from '../../Components/Header'
 import NewItems from '../../Components/NewItems'
 import RecentlyViewed from '../../Components/RecentlyViewed'
 import StatusOrder from '../../Components/StatusOrder'
 import CategoryHome from '../../Components/CategoryHome'
-
 export default function Home() {
+
   return (
     <div>
       <Header />
