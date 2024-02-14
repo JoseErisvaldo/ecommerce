470deff283f63df698876d5d6e67b641ea15dc1e
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-02-14 19:27:37 -0300

    feat: props

--- src/Components/StatusOrder/index.jsx
+++ src/Components/StatusOrder/index.jsx
@@ -1,18 +1,20 @@
 import './style.css'
 
-export default function StatusOrder() {
+export default function StatusOrder({componentUm, componentDois, componentTres}) {
   return (
     <div className="container-view-order">
       <h3>Status dos pedidos</h3>
+      
       <div className="card-status">
         <div className="order-status">
-          <div className="relative"></div>Pagar
+          <div className="relative"></div>
+          {componentUm}
         </div>
         <div className="order-status">
           <div className="relative"></div>
-          Receber
+          {componentDois}
         </div>
-        <div className="order-status">Rever</div>
+        <div className="order-status">{componentTres}</div>
       </div>
     </div>
   )
