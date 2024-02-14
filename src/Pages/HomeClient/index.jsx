810461f49809f449f0669e4a8a9c30d72cea3585
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-02-14 19:29:15 -0300

    feat: new Page clientHome

---
+++ src/Pages/HomeClient/index.jsx
@@ -0,0 +1,28 @@
+import Cards from '../../Components/Cards'
+import Header from '../../Components/Header'
+import NewItems from '../../Components/NewItems'
+import RecentlyViewed from '../../Components/RecentlyViewed'
+import StatusOrder from '../../Components/StatusOrder'
+import CategoryHome from '../../Components/CategoryHome'
+import './style.css'
+
+export default function HomeClient() {
+
+  return (
+    <div>
+      <Header />
+      <RecentlyViewed />
+      <StatusOrder componentUm={'Pagar'} componentDois={'Rever'} componentTres={'Devolução'} />
+      <NewItems
+        title={'Novos produtos'}
+        evento={'Ver mais'}
+        link={'/newitems'}
+      />
+      <CategoryHome
+        title={'Categorias'}
+        evento={'Ver mais'}
+        link={'/category'}
+      />
+    </div>
+  )
+}
