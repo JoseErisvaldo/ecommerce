ae9f43a6ce284206ba2e72296fd1a3339a165590
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-02-14 19:24:08 -0300

    feat: photo user

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -3,20 +3,41 @@ import { IoIosMenu } from 'react-icons/io'
 import { CiSettings } from 'react-icons/ci'
 import { BsTicketPerforated } from 'react-icons/bs'
 import NameUser from '../NameUser'
-import React, {useContext} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import { AuthContext } from '../../Contexts/Login'
+import supabaseClient from '../../supabaseClient'
 
 export default function Header() {
-  const {logout} = useContext(AuthContext)
+  const {logout, user} = useContext(AuthContext)
+  const [userBd, setUserBd] = useState([])
   const handelLogout = () => {
     logout()
   }
+
+  async function loadingUser () {
+    if(user) {
+      const {data, errror} = await supabaseClient
+    .from('users')
+    .select('*')
+    .eq('email' , user.email)
+    setUserBd(data)
+    }
+    
+  }
+
+  useEffect(() => {
+    loadingUser ()
+  },[user])
+
   return (
     <div>
       <div className="card-profile">
         <div className="card-dados">
           <div className="card-img-profile">
-            <img src="i" />
+            {userBd.map((photo) => (
+              <img key={photo.id} src={photo.photo} />
+            ))}
+            
           </div>
           <button className="btn-card">Minhas Atividades</button>
           <button onClick={handelLogout} className="btn-card">Sair</button>
