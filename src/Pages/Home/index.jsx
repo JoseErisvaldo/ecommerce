51e99c126c0872405f6eccb4a3fae491a6363e5a
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-02-14 19:28:54 -0300

    fix: exibição de autenticação

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,28 +1,34 @@
-import Announcement from '../../Components/Announcement'
+import Cards from '../../Components/Cards'
 import Header from '../../Components/Header'
 import NewItems from '../../Components/NewItems'
 import RecentlyViewed from '../../Components/RecentlyViewed'
 import StatusOrder from '../../Components/StatusOrder'
 import CategoryHome from '../../Components/CategoryHome'
+import './style.css'
+
 export default function Home() {
 
   return (
     <div>
       <Header />
-      <Announcement
-        tittle={'Anunciar'}
-        descripton={
-          'Promova seus produtos de maneira simples e eficaz. Anuncie agora !!'
-        }
-        link={'/announce'}
-      />
-      <Announcement
-        tittle={'Agendar'}
-        descripton={
-          'Promova seus produtos de maneira simples e eficaz. Anuncie agora !!'
-        }
-        link={'/schedule'}
-      />
+      <div className='card-home'>
+        <Cards
+          tittle={'Anunciar'}
+          descripton={
+            'Promova seus produtos de maneira simples e eficaz. Anuncie agora !!'
+          }
+          link={'/announce'}
+        />
+        <Cards
+          tittle={'Agendar'}
+          descripton={
+            'Promova seus produtos de maneira simples e eficaz. Anuncie agora !!'
+          }
+          link={'/schedule'}
+        />
+        <Cards tittle={'Ver agendamentos'} descripton={"Gerenciar meus agendamentos"} link={'/myschedule'} />
+        <Cards tittle={'Gerenciar estoque'} descripton={'Aqui voce gerencia seu estoque de forma facil e pratica !!'} link={'/stock'}/>
+      </div>
       <RecentlyViewed />
       <StatusOrder />
       <NewItems
