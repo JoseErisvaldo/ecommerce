df014191473ec39494857fed87cbb594b74ae584
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-02-16 21:45:53 -0300

    Feat: funcionalidade do Carinho

--- src/Pages/Products/index.jsx
+++ src/Pages/Products/index.jsx
@@ -7,12 +7,21 @@ import './style.css'
 import supabaseClient from "../../supabaseClient";
 import { useEffect, useState } from "react";
 import { SiGooglemaps } from "react-icons/si";
-import BtnCep from "../../Components/Btn/BtnArrow";
+import BtnCep from "../../Components/Btn/BtnCep";
+import BtnCart from "../../Components/Btn/BtnCart";
+import BtnFavorite from "../../Components/Btn/Favorite";
+import Star from "../../Components/Reviews/Star";
+import IconStar from "../../Components/Reviews/Icons/Star";
+import { GrFormSubtract } from "react-icons/gr";
+import { GrAdd } from "react-icons/gr";
 
 
 export default function Products () {
   const {idParams} = useParams()
   const [product, setProduct] = useState([])
+  const [size, setSize] = useState(null)
+  const [quantityInCart, setQuantityInCart] = useState(0);
+  const [qtd, setQtd] = useState(1)
 
   const fetchStock = () => {
     return new Promise((resolve, reject) => {
@@ -40,6 +49,50 @@ export default function Products () {
     })
   },[idParams])
 
+
+  function increment() {
+    const one = 1
+    setQtd( qtd + one)
+  }
+
+  function decrement() {
+    const one = 1
+    if(qtd >1 ) {
+      setQtd( qtd - one)
+    }
+    
+  }
+
+  const mapPhoto = product.map(item => {
+    return item.photo
+  })
+  const mapPrice = product.map(item => {
+    return item.price
+  })
+
+  const resCart = {
+    sku: idParams,
+    size: size,
+    photo: mapPhoto[0],
+    price: Number(mapPrice[0]),
+    qtd: qtd
+  };
+  
+  function handleCart() {
+    const getLocal = localStorage.getItem('@cartLojaMais');
+    let resLocal = JSON.parse(getLocal);
+    if (!Array.isArray(resLocal)) {
+      resLocal = [];
+    }
+    const hasRes = resLocal.some((item) => item.sku === resCart.sku)
+    if(hasRes) {
+      return alert('Item ja adicionado !')
+    }
+    resLocal.push(resCart);
+    localStorage.setItem('@cartLojaMais', JSON.stringify(resLocal));
+  }
+  
+ 
   return(
     <div>
       <Header/>
@@ -58,19 +111,52 @@ export default function Products () {
               <div className="products-img-center"><img src={item.photo} /></div>
             </div>
             <div className="card-dados-product">
-              <div className="products--description">Vendido por:JS Modas</div>
-              <div className="products--description">Entregue por: Loja Mais</div>
-              <div className="products--description">Descrição{item.description}</div>
-              <div className="products-price">Preço: {item.price}</div>
+              <div className="header-cart">
+                <div className="card-star">
+                  <div><Star starone={<IconStar/>}/></div>
+                  <div><Star starone={<IconStar/>}/></div>
+                  <div className="card-to-assess">Avaliar</div>
+                </div>
+                <div><BtnFavorite/></div>
+              </div>
+              <div className="products-vendido">Vendido por <span style={{ color: 'red' }}> JS Modas </span> </div>
+              <div className="products-entregue">Entregue por <span style={{ color: 'blue' }}> Loja Mais </span> </div>
+              <div className="products-description">{item.description}</div>
+              <div className="price-cart">
+                <span className="products-price-descount">R$ {item.price}</span>
+                <span className="products-price">R$ {item.price} no PIX</span>
+                <span className="products-price-parcelado">Ou 10x de R$ {item.price / 12} sem juros </span>
+              </div>
               <div className="products-cep">
                 <input placeholder="Digite seu CEP" type="number" name="cep" id="cep" />
                 <div className="icon-cep"><SiGooglemaps /></div>
                <BtnCep dados={'Buscar'}/>
               </div>
-              <div className="products-cart">Add ao carinho</div>
-              <div className="products-type">Tipo de entrega</div>
-              <div className="products-assessments">Avaliações</div>
-              <div className="products-comment">Comentarios</div>
+              <div>
+                <div className="products-type">Frete: R$: 12,99 - Sedex</div>
+                <div className="products-type">Frete: R$: 19,99 - Sedex-Express</div>
+              </div>
+              
+              <div className="products-cart">
+                <div>
+                  <select onChange={e => setSize(e.target.value)} className="select-size-cart">
+                    <option value={'-'}>Selecioanar Tamanho</option>
+                    <option value={'P'}>P</option>
+                    <option value={'PP'}>PP</option>
+                    <option value={'M'}>M</option>
+                    <option value={'G'}>G</option>
+                  </select>
+                </div>
+                <div className="qtd-order">
+                  <div onClick={decrement} className="decrement-cart"><GrFormSubtract /></div>
+                  <div  className="res-cart">{qtd}</div>
+                  <div onClick={increment} className="increment-cart"><GrAdd /></div>
+                  <button onClick={handleCart} className="btn-cart">Adicionar</button>
+                </div> 
+              </div>
+              
+              {/*<div className="products-assessments">Avaliações</div>
+              <div className="products-comment">Comentarios</div>*/}
             </div>
           </div>
         ))}
